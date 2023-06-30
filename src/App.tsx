38e08d5c463c510795726db1e1cf2ede0feb275e
alztr2908 <alztr2908@gmail.com>
2023-07-01 02:41:46 +0800

    finished styling footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,17 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <NavBar />
-      {/* <div
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      > */}
-      <div className="container-sm mx-auto border border-primary-subtle rounded-3">
-        <Routes>
-          <Route path="/" element={<Current />} />
-          <Route path="/overall" element={<Overall />} />
-          <Route path="/legend" element={<Legend />} />
-        </Routes>
+      <div style={{ minHeight: "100vh" }}>
+        <NavBar />
+        <div className="container-sm mx-auto border border-primary-subtle rounded-3 px-3">
+          <Routes>
+            <Route path="/" element={<Current />} />
+            <Route path="/overall" element={<Overall />} />
+            <Route path="/legend" element={<Legend />} />
+          </Routes>
+        </div>
+        {/* </div> */}
       </div>
-      {/* </div> */}
       <Footer />
     </>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer
+        className="footer"
+        style={{
+          padding: "20px",
+          textAlign: "center",
+          position: "relative",
+          bottom: "0",
+          width: "100%",
+          marginTop: "1em",
+          backgroundColor: "#85ce2f",
+        }}
+      >
+        <div className="container">
+          <span className="text-muted">
+            Created by Alzter Aquino as a requirement for EEE 192
+          </span>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
